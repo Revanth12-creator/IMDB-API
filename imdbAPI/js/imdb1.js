1a3dfff71646a6a09a88c6343ae0ac735796b04f
Revanth12-creator <65896457+Revanth12-creator@users.noreply.github.com>
2020-06-02 14:38:44 +0530

    add files via upload

---
+++ imdbAPI/js/imdb1.js
@@ -0,0 +1,74 @@
+let search=document.querySelector('#search');
+search.addEventListener('keyup',(e)=>{
+    let searchText=e.target.value;
+    console.log(searchText);
+    SearchMovies(searchText);
+    //when key press hide from text and h1
+    let formText=document.getElementById("divBlock");
+    formText.style.display="none";
+    search.classList.add("afterkeyPress");
+    document.querySelector("#formBlock").classList.add("afterkey_formBlock");
+});
+//speechRecognition Api
+let speechSearch=document.getElementById("speechIcon");
+speechSearch.addEventListener("click",()=>{
+    let formText=document.getElementById("divBlock");
+    formText.style.display="none";
+    search.classList.add("afterkeyPress");
+    document.querySelector("#formBlock").classList.add("afterkey_formBlock");
+
+    window.SpeechRecognition=
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+    let recognition=new SpeechRecognition();
+    let p = document.createElement("p");
+    recognition.interimResults=true;
+
+    recognition.addEventListener("result",(e)=>{
+        let transcript=[...e.results]
+        .map((result)=>result[0])
+        .map((result)=>result.transcript)
+        .join("");
+        search.value=transcript;
+        if(e.results[0].isFinal){
+            p=document.createElement("p");
+            p.innerHTMl=transcript;
+            let searchText=transcript;
+            SearchMovies(searchText);
+        }
+
+    });
+    recognition.start();
+});
+
+function SearchMovies(searchText){
+    const imdbapi=`http://www.omdbapi.com/?s=${searchText}&apikey=f056e2f7`;
+    window
+    .fetch(imdbapi)
+    .then(data=>{
+        data
+        .json()
+        .then((movieData)=>{
+            let movies=movieData.Search;
+            let output=[];
+            for(let movie of movies){
+                let defaultImg=
+                movie.Poster==="N/A"
+                ?"https://eticketsolutions.com/demo/themes/e-ticket/img/movie.jpg"
+                :movie.Poster;
+                output+=`
+                <div>
+                <img src="${defaultImg}"/>
+                <h1>${movie.Title}</h1>
+                <p>${movie.Year}</p>
+                <a href="http://www.imdb.com/title/${movie.imdbID}/" target="_blank">Movie Details</a>
+                </div>
+                `;
+            }
+            document.getElementById("template").innerHTML=output;
+            
+        })
+        .catch(err=>console.log(err));
+    })
+.catch((err)=>console.log(err));
+    
+}
